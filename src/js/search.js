40890aee08a14391e88a3adec8c3900f760c4422
Octavio Martinez <omartinez1618@gmail.com>
2023-10-01 20:41:53 -0400

    ajustes en estilos de iframe

--- src/js/search.js
+++ src/js/search.js
@@ -4,6 +4,11 @@
         list = document.querySelector("#user-repositories-list")
         const search = document.createElement("iframe")
         search.style.border = "none"
+        search.style.width = "100%"
+        search.style.backgroundColor = "transparent"
+        search.style.height = "100px"
+        search.style.margin = "0px";
+        search.style.padding = "0px";
         search.src = chrome.runtime.getURL("src/html/search.html")
         list.before( search )
     })
