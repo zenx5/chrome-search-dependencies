97eb0930813b2ec540698f3029a6a38f8ad2939b
Javierjmc <javierjosemartinezcedeno@gmail.com>
2023-10-09 14:50:04 -0400

    funcionalidad search agregada

--- src/js/search.js
+++ src/js/search.js
@@ -6,7 +6,7 @@
         search.style.border = "none"
         search.style.width = "100%"
         search.style.backgroundColor = "transparent"
-        search.style.height = "100px"
+        search.style.height = "150px"
         search.style.margin = "0px";
         search.style.padding = "0px";
         search.src = chrome.runtime.getURL("src/html/search.html")
