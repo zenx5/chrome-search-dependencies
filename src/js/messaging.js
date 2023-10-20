8b47f41a6e2cbf4345dbe0f93807eddae7223f7f
Octavio Martinez <omartinez1618@gmail.com>
2023-10-20 15:18:02 -0400

    message con runtime en lugar de port

--- src/js/messaging.js
+++ src/js/messaging.js
@@ -1,14 +1,4 @@
-
-window.port = chrome.runtime.connect({
-    name: Math.floor( Math.random()*99 )+"-"+Math.floor( Math.random()*99 )
-});
-
-window.port.onMessage.addListener( response => {
-    switch( response.action ) {
-        case ERROR:
-            console.log(response.error)
-            document.location.reload()
-            break;
-
-    }
-});
+chrome.runtime.onMessage.addListener(function( message, sender ){
+    console.log('messaging')
+    console.log(message, sender)
+})
