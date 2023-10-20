59265b98b92b565a52717e3048ddda31e02c0cb5
Octavio Martinez <omartinez1618@gmail.com>
2023-10-20 15:15:42 -0400

    escucha para mensajes y try-catch para suscripcion

--- background.js
+++ background.js
@@ -2,14 +2,23 @@
 chrome.runtime.onInstalled.addListener( async function(){
     const extension = await chrome.management.getSelf()
     const url = "http://localhost:3000/api/extension"
-
-    const response = await fetch( url, {
-        method:'post',
-        body: JSON.stringify({
-            code: extension.id,
-            navigator: "chrome"
+    try {
+        const response = await fetch( url, {
+            method:'post',
+            body: JSON.stringify({
+                code: extension.id,
+                navigator: "chrome"
+            })
         })
-    })
-    const suscription = await response.json()
-    console.log( suscription )
+        const suscription = await response.json()
+        console.log( suscription )
+    } catch(error) {
+        console.log( error )
+    }
 });
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    console.log('resend')
+    chrome.tabs.sendMessage( sender.tab.id, message )
+
+})
